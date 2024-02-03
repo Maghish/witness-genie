7da6e6ad2b98068ca7d1958e93d9eca92eaa8468
Maghish <mail4mithran@gmail.com>
2024-02-03 13:25:54 +0530

    Added Commets

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,28 @@
-require('dotenv').config()
+// require all the packages
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 
-
+// initialize express app
 const app = express()
 
+// middleware
 app.use((req, res, next) => {
     console.log(req.method, req.url);
     next();
 })
 
+// return json response on '/' route
 app.get('/', (req, res) => {
     res.json({msg: "Hello World!"});
 })
 
+// connect to mongodb 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
+    // log out a message saying that connected to mongodb successfully
+    console.log("Connected to MongoDB successfully")
+    // start listening to port 
     app.listen(process.env.PORT , () => {
         console.log(`listening on port ${process.env.PORT}`)
     })
